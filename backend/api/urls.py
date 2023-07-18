fe17453551fdeef4bdb3177d7cedda4720d4fb72
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-07-18 15:09:51 +0700

    add cors

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('notes/', views.get_notes, name="notes"),
-    path('note/create/', views.create_note, name="create_note"),
-    path('note/<str:pk>/update/', views.update_note, name="update_note"),
-    path('note/<str:pk>/delete/', views.delete_note, name="delete_note"),
-    path('note/<str:pk>/', views.get_note, name="note details"),
+    path('notes/create/', views.create_note, name="create_note"),
+    path('notes/<str:pk>/update/', views.update_note, name="update_note"),
+    path('notes/<str:pk>/delete/', views.delete_note, name="delete_note"),
+    path('notes/<str:pk>/', views.get_note, name="note details"),
     # Shared Note URL pattern
     path('notes/shared/', views.get_notes_shared, name="shared"),
 
