78e1ee71b4d201380e0d2aa77a5f1b0900b5f731
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-07-18 15:46:12 +0700

    Update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEYS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -114,7 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # MEDIA
 MEDIA_URL = '/media/'
